0454209688a4cc2671e5830bf1323b22b8f68c40
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:28:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -19,3 +19,20 @@ array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split(" ").first
 end
+
+
+
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
+ 
+# => ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
+
+
+
+bundle
+
+install 
+
