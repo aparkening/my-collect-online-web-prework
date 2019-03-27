b50fab6c83b0bc1faa5184ed73f2a31e6debb72f
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:39:57 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 # Take an argument of a collection, iterate through the collection feeding each element to the code block, then return the modified collection.
 def my_collect(collection)
+  new_collection = [ ]
   i = 0
   while i < collection.length
-    collection[i] = yield(collection[i])
+    new_collection[i] = yield(collection[i])
     i += 1
   end
-  collection                   # Return modified array
+  new_collection                   # Return modified array
 end
