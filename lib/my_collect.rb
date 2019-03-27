7f60b7c14a749fd19ee2fc8a20ed0a1f2ad9b840
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:25:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,2 @@
-
+# Take an argument of a collection, iterate through that collection, then execute the code in the block you call it with for each element in the collection (use the yield keyword). It should return the modified collection.
 
