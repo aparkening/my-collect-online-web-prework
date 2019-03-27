8d907ba70688e1109a7948ecd47cbcc6cdff8e69
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-27 16:27:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
-# Take an argument of a collection, iterate through that collection, then execute the code in the block you call it with for each element in the collection (use the yield keyword). It should return the modified collection.
+# Take an argument of a collection, iterate through that collection, then execute the code in the block for each element in the collection. Return the modified collection.
 
+
+def my_collect(collection)
+  
+  
+  Iterate through collection
+  
+  yield to block
+  
+  return modified collection
+  
+  
+end
+
+call block
